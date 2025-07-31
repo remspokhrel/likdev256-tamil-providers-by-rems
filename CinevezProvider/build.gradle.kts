3b7bb66ef5981b767aa9a0995b2f1238e7e1a174
remspokhrel <remspokhrel@gmail.com>
2025-08-01 00:13:58 +0530

    Update build.gradle.kts
    Signed-off-by: remspokhrel <remspokhrel@gmail.com>

--- CinevezProvider/build.gradle.kts
+++ CinevezProvider/build.gradle.kts
@@ -21,5 +21,5 @@ cloudstream {
         "Movie",
     )
 
-    iconUrl = "https://www.google.com/s2/favicons?domain=ww1.cinevez.mx&sz=%size%"
+    iconUrl = "https://www.google.com/s2/favicons?domain=wwv-fmovies.com&sz=%size%"
 }
